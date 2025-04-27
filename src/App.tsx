f558080707be4e553be3b1c9866f901e0b8c8b1f
sarar <57088925+s6439sr@users.noreply.github.com>
2025-04-27 09:31:15 +0200

    Revert "Reapply "Update App.tsx""
    This reverts commit 98d7c67a101a1b5de2e2de15a3791558a162a429.

--- src/App.tsx
+++ src/App.tsx
@@ -21,20 +21,13 @@ function App() {
     client.models.Todo.create({ content: window.prompt("Todo content") });
   }
 
-    
-  function deleteTodo(id: string) {
-    client.models.Todo.delete({ id })
-  }
-
   return (
     <main>
       <h1>My todos</h1>
       <button onClick={createTodo}>+ new</button>
       <ul>
         {todos.map((todo) => (
-          <li           
-          onClick={() => deleteTodo(todo.id)}
-          key={todo.id}>{todo.content}</li>
+          <li key={todo.id}>{todo.content}</li>
         ))}
       </ul>
       <div>
